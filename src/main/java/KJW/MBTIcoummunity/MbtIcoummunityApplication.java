7605c24bda5a04ad0a1a61ff3abc4989ee986c8b
KongJunWoo <joonoo3@naver.com>
2023-07-27 22:17:58 +0900

    feat : 초기 환경설정

---
+++ src/main/java/KJW/MBTIcoummunity/MbtIcoummunityApplication.java
@@ -0,0 +1,13 @@
+package KJW.MBTIcoummunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtIcoummunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtIcoummunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KJW/MBTIcoummunity/MbtIcoummunityApplicationTests.java
@@ -0,0 +1,13 @@
+package KJW.MBTIcoummunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtIcoummunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
