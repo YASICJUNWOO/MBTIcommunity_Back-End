3451ab24387f56f43dccab3064b7b6259aee7306
KongJunWoo <joonoo3@naver.com>
2023-09-05 12:32:21 +0900

    fix : CORS 해결

--- src/main/java/KJW/MBTIcoummunity/Security/LoginController.java
+++ src/main/java/KJW/MBTIcoummunity/Security/LoginController.java
@@ -8,9 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/login")
@@ -28,4 +29,5 @@ public class LoginController {
         logger.warn("로그인 실패");
         return new ResponseEntity<>("로그인 실패",HttpStatus.EXPECTATION_FAILED);
     }
+
 }
