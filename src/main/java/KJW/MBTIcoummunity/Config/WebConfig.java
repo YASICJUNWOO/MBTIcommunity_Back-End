f33945c4cd1ff3653a19d9f8c7301be981e06671
KongJunWoo <joonoo3@naver.com>
2023-09-05 12:56:15 +0900

    fix : 코드 추가

--- src/main/java/KJW/MBTIcoummunity/Config/WebConfig.java
+++ src/main/java/KJW/MBTIcoummunity/Config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE","PATCH");
     }
 }
